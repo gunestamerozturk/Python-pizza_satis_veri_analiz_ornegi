a548abdb925b744018e99d4a89babae15e32348c
Güneş Tamer Öztürk <58487297+gunestamerozturk@users.noreply.github.com>
2025-12-26 15:19:25 +0300

    Update restoran.py
    Açıklama eklendi ve satır düzenlemeleri yapıldı.

--- restoran.py
+++ restoran.py
@@ -5,21 +5,25 @@ from restoran_view import *
 # Excel dosya yolu
 EXCEL_DOSYASI = "veriler.xlsx"
 
+#Excel dosyasını okur, df adında veri çerçevesine tabloyu tanımlar ve df değerini döndürür.
 def excel_oku(restoran_ad):
-    df = pd.read_excel(EXCEL_DOSYASI)
-    df = df[df["Restaurant Name"]==restoran_ad]
+    df = pd.read_excel(EXCEL_DOSYASI) #pandas kütüphanesi kullanılarak excel dosyası df değişkenine tanımlandı.
+    df = df[df["Restaurant Name"]==restoran_ad] #excel dosyasından gelen veri, eşitlik kontrolü yapılarak filtrelendi.
     return df
-    
+
+#Veri çerçevesindeki toplam kayıt sayısını bulur.    
 def rst_toplam_satis(restoran_ad):
     df = excel_oku(restoran_ad)
-    toplam = df.shape[0]
+    toplam = df.shape[0] #shape[0] satır sayısını verir.
     return toplam
 
+#Veri çerçevesindeki "Delivery Duration (min)" sütunundaki değerlerin ortalamasını alır. 
 def rst_ortalama_teslimat_suresi(restoran_ad):
     df = excel_oku(restoran_ad)
-    ortalama = df["Delivery Duration (min)"].mean()
+    ortalama = df["Delivery Duration (min)"].mean() #mean() fonksiyonu sayısal değerlerin ortalamasını alır.
     return round(ortalama,2)
 
+#Veri çerçevesinde "Is Delayed" sütunundaki True değerlerinin toplam değer sayısına oranını bulur.
 def rst_gecikme_orani(restoran_ad):
     df = excel_oku(restoran_ad)
     toplam = len(df)
@@ -27,32 +31,35 @@ def rst_gecikme_orani(restoran_ad):
     oran = (geciken / toplam) * 100
     return round(oran,2)
 
+#Veri çerçevesindeki "Location" sütununda gruplama ve sayım yaparak en yüksek değeri bulur
 def rst_en_cok_satan_sube(restoran_ad,lokasyon = "Location"):
     df = excel_oku(restoran_ad)
-    yuksek_satis = df.groupby([lokasyon]).size().reset_index(name="satis_sayisi")
-    max_satis = yuksek_satis['satis_sayisi'].max()
-    en_yuksek_satis_sube = yuksek_satis[yuksek_satis['satis_sayisi']==max_satis].iloc[0]
+    yuksek_satis = df.groupby([lokasyon]).size().reset_index(name="satis_sayisi") #Lokasyon sütunundaki değerleri gruplar, size() ile veri sayısını alır, reset_index() ile sonucu veri çerçevesine dönüştürür.
+    max_satis = yuksek_satis['satis_sayisi'].max() #max() en yüksek değeri bulur.
+    en_yuksek_satis_sube = yuksek_satis[yuksek_satis['satis_sayisi']==max_satis].iloc[0] #En yüksek değere sahip "Location" değerini filtreler.
     return {
         'Location':en_yuksek_satis_sube["Location"],
         'satis_sayisi':int(en_yuksek_satis_sube['satis_sayisi'])
     }
 
+#Veri çerçevesindeki "Location" sütununda gruplama ve sayım yaparak en düşük değeri bulur.
 def rst_en_az_satan_sube(restoran_ad,lokasyon = "Location"):
     df = excel_oku(restoran_ad)
     yuksek_satis = df.groupby([lokasyon]).size().reset_index(name="satis_sayisi")
-    min_satis = yuksek_satis['satis_sayisi'].min()
+    min_satis = yuksek_satis['satis_sayisi'].min() #min() endüşük değeri bulur.
     en_dusuk_satis_sube = yuksek_satis[yuksek_satis['satis_sayisi']==min_satis].iloc[0]
     return {
         'Location':en_dusuk_satis_sube["Location"],
         'satis_sayisi':int(en_dusuk_satis_sube['satis_sayisi'])
     }
 
+#Veri çerçevesindeki "Location" ve "Is Delayed" sütunlarında gruplama ve filtreleme işlemleri yaparak en düşük değeri bulur.
 def rst_gecikme_orani_en_dusuk_sube(restoran_ad,gecikme="Is Delayed",lokasyon="Location"):
     df = excel_oku(restoran_ad)
     df["geciken"]=df["Is Delayed"] == True
-    gecikme_orani = (df.groupby(lokasyon)[gecikme].mean().mul(100).round(2))
+    gecikme_orani = (df.groupby(lokasyon)[gecikme].mean().mul(100).round(2)) #mul() çarpma işlemi yapar.
 
-    en_az_geciken_restoran = gecikme_orani.idxmin()
+    en_az_geciken_restoran = gecikme_orani.idxmin() #idxmin() minimum değerin konumunu bulur.
     en_az_geciken_oran = gecikme_orani.min()
 
     return{
@@ -60,12 +67,13 @@ def rst_gecikme_orani_en_dusuk_sube(restoran_ad,gecikme="Is Delayed",lokasyon="L
         "gecikme_orani":float(en_az_geciken_oran)
     }
 
+#Veri çerçevesindeki "Location" ve "Is Delayed" sütunlarında gruplama ve filtreleme işlemleri yaparak en yüksek değeri bulur.
 def rst_gecikme_orani_en_yuksek_sube(restoran_ad,gecikme="Is Delayed",lokasyon="Location"):
     df = excel_oku(restoran_ad)
     df["geciken"]=df["Is Delayed"] == True
     gecikme_orani = (df.groupby(lokasyon)[gecikme].mean().mul(100).round(2))
 
-    en_cok_geciken_restoran = gecikme_orani.idxmax()
+    en_cok_geciken_restoran = gecikme_orani.idxmax() #idxmax() maksimum değerin konumunu bulur.
     en_cok_geciken_oran = gecikme_orani.max()
 
     return{
@@ -73,6 +81,7 @@ def rst_gecikme_orani_en_yuksek_sube(restoran_ad,gecikme="Is Delayed",lokasyon="
         "gecikme_orani":float(en_cok_geciken_oran)
     }
 
+#Veri çerçevesindeki "Payment Method" sütununda gruplama ve filtreleme işlemleri yaparak en yüksek değeri bulur.
 def rst_odeme_yontemi(restoran_ad,odeme_yontemi = "Payment Method"):
     df = excel_oku(restoran_ad)
     odeme_yontemleri = df.groupby([odeme_yontemi]).size().reset_index(name="satis_sayisi")
@@ -83,12 +92,13 @@ def rst_odeme_yontemi(restoran_ad,odeme_yontemi = "Payment Method"):
         "satis_sayisi":en_cok_tercih_edilen_odeme["satis_sayisi"]
     }
 
+#Veri çerçevesindeki "Payment Category" sütununda gruplama ve filtreleme işlemleri yaparak en yüksek değeri bulur.
 def rst_siparis_yontemi(restoran_ad,siparis_yontemi = "Payment Category"):
     df = excel_oku(restoran_ad)
     toplam = len(df)
     online = len(df[df[siparis_yontemi]=="Online"])
     online_oran = (online / toplam) * 100
-    if online_oran > 50:
+    if online_oran > 50:   #Koşul blokları kullanılarak farklı sonuçlar döndürülmesi sağlandı.
         return {
             'Oran':round(online_oran,2),
             'Method':"Online"
@@ -103,16 +113,18 @@ def rst_siparis_yontemi(restoran_ad,siparis_yontemi = "Payment Category"):
             'Oran':round((100-online_oran),2),
             'Method':"Online/Offline"
         }
-    
+
+#Veri çerçevesindeki "Order Hour" sütununda gruplama ve filtreleme işlemleri yapar ve değerleri listeler   
 def rst_saatlik_satis_grafigi(restoran_ad,siparis_saati = "Order Hour"):
     df = excel_oku(restoran_ad)
     yogunluk = df[siparis_saati].value_counts().rename_axis("Saat").reset_index(name="Siparis Sayisi").sort_values("Saat")
+    #value_counts() verinin tekrar sayısını bulur. rename_axis() eksen ismini değiştirir. sort_values() değerlere göre sıralama yapar.
     return{
         "Saatler":yogunluk["Saat"].tolist(),
         "Siparisler": yogunluk["Siparis Sayisi"].tolist()
     }
 
-#Aylık Satış Grafiği
+#Veri çerçevesindeki "Order Month" sütununda gruplama, sıralama ve filtreleme işlemleri yapar ve değerleri listeler 
 def rst_aylik_satis_grafigi(restoran_ad,month = "Order Month"):
     df = excel_oku(restoran_ad)
     ay_sirasi = {
@@ -131,16 +143,16 @@ def rst_aylik_satis_grafigi(restoran_ad,month = "Order Month"):
     }
 
     aylik_satis = (df[month].value_counts().rename_axis("Ay").reset_index(name="Satis"))
-    aylik_satis["Sira"]=aylik_satis["Ay"].map(ay_sirasi)
+    aylik_satis["Sira"]=aylik_satis["Ay"].map(ay_sirasi) #map() sıralama işlemi yapar.
     aylik_satis = aylik_satis.sort_values("Sira")
 
     return {
         'aylar': aylik_satis["Ay"].tolist(),
         'satislar':aylik_satis["Satis"].tolist(),
-        'toplam':aylik_satis.sum()
+        'toplam':aylik_satis.sum() #sum() toplama işlemi yapar.
     }
 
-#Pizza Tipi Dağılımı
+#Veri çerçevesindeki "Pizza Type" sütununda gruplama, sıralama ve filtreleme işlemleri yapar ve değerleri listeler
 def rst_pizza_tipi_grafigi(restoran_ad,pizza_type="Pizza Type"):
     df = excel_oku(restoran_ad)
     pizza_tipleri = {
@@ -164,5 +176,5 @@ def rst_pizza_tipi_grafigi(restoran_ad,pizza_type="Pizza Type"):
     return{
         'Pizza Tipleri':pizza_tipi["Pizza Tipi"].tolist(),
         'satislar':pizza_tipi["Satis"].tolist(),
-        'toplam':pizza_tipi.sum()
+        'toplam':pizza_tipi.sum() 
     }
